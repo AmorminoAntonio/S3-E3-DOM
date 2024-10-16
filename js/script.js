6faf982d47230a193b27e6c7d5a44cb06047d58c
Antonio Amormino <crisantoamo@gmail.com>
2024-10-16 20:39:25 +0200

    modified_post solution view

--- js/script.js
+++ js/script.js
@@ -1,35 +1,48 @@
-document.getElementById('add-btn').addEventListener('click', function() {
-    const input = document.getElementById('todo-input');
-    const todoText = input.value.trim();
 
-    if (todoText !== '') {
-        addTodoItem(todoText);
-        input.value = ''; // svuota l'input
-    } else {
-        alert('Inserisci un "attività prima di aggiungerla!" ');
+const handleSubmit = function (event) {
+    event.preventDefault() // serve ad evitare il refresh pagine dopo l'esecuzione (invio form, click aggiungi, ecc..)
+    addTask()  // richiamo della funzione
+    attachComplete()  // richiamo della funzione
+    attachDelete()  // richiamo della funzione
+  }
+  
+  const addTask = function () {
+    let tasks = document.querySelector('#tasks')
+    let inputField = document.querySelector('#newTask input')
+    let newTask = `
+        <div class="task">
+            <span id="taskname">
+                ${inputField.value}  <!-- $ questo simbolo serve a specificare il richiamo implicito ad un dato annidato o no.-->
+            </span>
+            <button class="delete">
+                <i class="far fa-trash-alt"></i>
+            </button>
+        </div>
+        `
+    tasks.innerHTML += newTask
+    inputField.value = ''
+  }
+  
+  const attachComplete = function () {
+    let allTasks = document.querySelectorAll('.task')
+    for (let i = 0; i < allTasks.length; i++) {
+      allTasks[i].addEventListener('click', function () {
+        this.classList.toggle('completed')
+      })
     }
-});
-
-function addTodoItem(text) {
-    const list = document.getElementById('todo-list');
-
-    // Crea un nuovo elemento <li>
-    const todoItem = document.createElement('li');
-    const todoSpan = document.createElement('span');
-    todoSpan.textContent = text;
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Elimina';
-    deleteBtn.classList.add('delete-btn');
-
-    // Gestisci la rimozione dell'attività
-    deleteBtn.addEventListener('click', function() {
-        list.removeChild(todoItem);
-    });
-
-    // Aggiungi il testo e il pulsante Elimina all'elemento <li>
-    todoItem.appendChild(todoSpan);
-    todoItem.appendChild(deleteBtn);
-
-    // Aggiungi il nuovo elemento <li> alla lista
-    list.appendChild(todoItem);
-}
+  }
+  
+  const attachDelete = function () {
+    let allDeleteButtons = document.querySelectorAll('.delete')
+    for (let i = 0; i < allDeleteButtons.length; i++) {
+      allDeleteButtons[i].addEventListener('click', function () {
+        this.parentNode.remove()
+      })
+    }
+  }
+  
+  window.onload = function () {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', handleSubmit)
+  }
+  
